92eaef146fda63af539f0f2d83b7a559d434e3e9
Michal Cieslakiewicz <michal.cieslakiewicz@wp.pl>
2020-06-01 23:00:26 +0200

    ap/buildah added, version 1.14.9.
    Buildah container builder added, version 1.14.9 matches podman 1.9.3.
    
    Signed-off-by: Michal Cieslakiewicz <michal.cieslakiewicz@wp.pl>

---
+++ ap/buildah/get_sources.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+SRC=buildah
+VERSION=1.14.9
+git clone -b master https://github.com/containers/${SRC}.git $SRC && \
+  ( cd $SRC && git co v${VERSION} )
+mv $SRC ${SRC}-${VERSION}
+tar cf - ${SRC}-${VERSION} | xz -c9 > ${SRC}-${VERSION}.tar.xz
+[ -s ${SRC}-${VERSION}.tar.xz ] && rm -rf ${SRC}-${VERSION}
