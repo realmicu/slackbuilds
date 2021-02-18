515a6d8bb782ebc6d1d29eed5602a45e83562955
Michal Cieslakiewicz <michal.cieslakiewicz@wp.pl>
2021-02-18 13:28:59 +0100

    xfce/xfce4-whiskermenu-plugin: removed.
    Now part of official Slackware-current.
    
    Signed-off-by: Michal Cieslakiewicz <michal.cieslakiewicz@wp.pl>

--- xfce/xfce4-whiskermenu-plugin/doinst.sh
+++
@@ -1,2 +0,0 @@
-[ -x usr/bin/update-desktop-database ] && usr/bin/update-desktop-database
-[ -x usr/bin/gtk-update-icon-cache ] && usr/bin/gtk-update-icon-cache -t -f usr/share/icons/hicolor
--- xfce/xfce4-whiskermenu-plugin/get_sources.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-SRC=xfce4-whiskermenu-plugin
-VERSION=2.4.3
-set -x
-wget -c https://git.xfce.org/panel-plugins/${SRC}/snapshot/${SRC}-${VERSION}.tar.gz
