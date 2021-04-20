bdcd7d5900652828d30a205d4786ea9a25735961
Michal Cieslakiewicz <michal.cieslakiewicz@wp.pl>
2021-04-20 12:42:33 +0200

    Nvidia legacy driver packages updated to version 390.143.
    Nvidia legacy binary driver updated to version 390.143.
    
    Affected packages:
    
      l/nvidia-driver - no changes to build script, new package
      provides newer versions of libraries included in 390.141
      only.
    
      k/kmod-nvidia - this kernel driver builds successfully for
      kernels 5.11 with no patches required.
    
    Performance and suspend/resume tested - works as expected.
    
    Signed-off-by: Michal Cieslakiewicz <michal.cieslakiewicz@wp.pl>

--- l/nvidia-driver/get_sources.sh
+++ l/nvidia-driver/get_sources.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -x
 BINDIST=NVIDIA-Linux-x86_64
-VERSION=390.138
+VERSION=390.143
 wget https://us.download.nvidia.com/XFree86/Linux-x86_64/${VERSION}/${BINDIST}-${VERSION}.run && \
   chmod +x ${BINDIST}-${VERSION}.run
