d9d456d3cedf0ee32a24f18a94a22df705223e39
Michal Cieslakiewicz <michal.cieslakiewicz@wp.pl>
2021-12-28 21:12:15 +0100

    l/virglrenderer: updated to 9.1.0.
    Build system has changed from autotools to meson.
    
    Signed-off-by: Michal Cieslakiewicz <michal.cieslakiewicz@wp.pl>

--- l/virglrenderer/get_sources.sh
+++ l/virglrenderer/get_sources.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 SRC=virglrenderer
-BRANCH=master
-git clone -b $BRANCH --depth 1 https://gitlab.freedesktop.org/virgl/${SRC}.git/ $SRC
-VERSION=`sed -e 's/\[/ /g' -e 's/\]/ /g' -e 's/,/ /g' -e 's/(/ /g' $SRC/configure.ac | awk -v DATE=\`date '+%Y%m%d'\` '$1=="AC_INIT" && $2=="virglrenderer" { printf "%s_%sgit\n", $NF, DATE }'`
-chown -R root:root $SRC
-mv $SRC ${SRC}-${VERSION}
+VERSION=0.9.1
+set -x
+wget -c https://gitlab.freedesktop.org/virgl/$SRC/-/archive/${SRC}-${VERSION}/${SRC}-${SRC}-${VERSION}.tar.bz2 || exit 1
+tar xjf ${SRC}-${SRC}-${VERSION}.tar.bz2 && rm -f ${SRC}-${SRC}-${VERSION}.tar.bz2
+mv ${SRC}-${SRC}-${VERSION} ${SRC}-${VERSION}
+chown -R root:root ${SRC}-${VERSION}
 tar cf - ${SRC}-${VERSION} | xz -c9 > ${SRC}-${VERSION}.tar.xz
 [ -s ${SRC}-${VERSION}.tar.xz ] && rm -rf ${SRC}-${VERSION}
