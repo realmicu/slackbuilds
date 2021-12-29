c1f8f20d06f5cc59b4394d95e3e6e0c1203ea1d3
Michal Cieslakiewicz <michal.cieslakiewicz@wp.pl>
2021-12-29 23:08:15 +0100

    ap/crun: updated to current (1.4.16_fafb).
    Signed-off-by: Michal Cieslakiewicz <michal.cieslakiewicz@wp.pl>

--- ap/crun/get_sources.sh
+++ ap/crun/get_sources.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 SRC=crun
-git clone -b master https://github.com/containers/${SRC}.git $SRC
+git clone -b main --recurse-submodules https://github.com/containers/${SRC}.git $SRC
 VERSION=$(cd $SRC && ./build-aux/git-version-gen --prefix "" /dev/null | sed -e 's/-/_/g')
 mv $SRC ${SRC}-${VERSION} 
 tar cf - ${SRC}-${VERSION} | xz -c9 > ${SRC}-${VERSION}.tar.xz
