52af4c5664831b8a43c9d5d774365dc7c1144f75
Michal Cieslakiewicz <michal.cieslakiewicz@wp.pl>
2021-12-29 23:43:18 +0100

    n/cni-plugins: updated to 1.0.1.
    Signed-off-by: Michal Cieslakiewicz <michal.cieslakiewicz@wp.pl>

--- n/cni-plugins/get_sources.sh
+++ n/cni-plugins/get_sources.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 SRC=cni-plugins
-git clone -b master https://github.com/containernetworking/plugins.git $SRC
-VERSION=$(cd $SRC && git describe --tags --dirty | sed -e 's/v//' -e 's/-/_/g')
-mv $SRC ${SRC}-${VERSION}
+VERSION=1.0.1
+set -x
+wget -c -O ${SRC}-${VERSION}.tar.gz https://github.com/containernetworking/plugins/archive/refs/tags/v${VERSION}.tar.gz || exit 1
+tar xzf ${SRC}-${VERSION}.tar.gz && rm -f ${SRC}-${VERSION}.tar.gz
+mv plugins-${VERSION} ${SRC}-${VERSION}
+chown -R root:root ${SRC}-${VERSION} 2>/dev/null
 tar cf - ${SRC}-${VERSION} | xz -c9 > ${SRC}-${VERSION}.tar.xz
 [ -s ${SRC}-${VERSION}.tar.xz ] && rm -rf ${SRC}-${VERSION}
