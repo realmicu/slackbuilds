0c985d7389d8e68b28120ff99094e73f878c8098
Michal Cieslakiewicz <michal.cieslakiewicz@wp.pl>
2022-01-05 17:12:14 +0100

    ap/buildah: updated to current dev version (1.24.0-dev).
    This fixes buildah crashes when creating images.
    Also it is up-to-date with dev branch of podman.
    
    Signed-off-by: Michal Cieslakiewicz <michal.cieslakiewicz@wp.pl>

--- ap/buildah/get_sources.sh
+++ ap/buildah/get_sources.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 SRC=buildah
-VERSION=1.23.1
 # Build requires a proper git tree:
-git clone -b main https://github.com/containers/${SRC}.git $SRC && \
-  ( cd $SRC && git co v${VERSION} )
+git clone -b main https://github.com/containers/${SRC}.git $SRC
+VERSION=$(awk '$1=="Version" { print $NF }' $SRC/define/types.go | sed -e 's/-/_/' -e 's/"//g')_$(cd $SRC && git log -n 1 --format=%h --abbrev=8)
 mv $SRC ${SRC}-${VERSION}
 tar cf - ${SRC}-${VERSION} | xz -c9 > ${SRC}-${VERSION}.tar.xz
 [ -s ${SRC}-${VERSION}.tar.xz ] && rm -rf ${SRC}-${VERSION}
