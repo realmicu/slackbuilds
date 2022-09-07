19fdcdcf27455c592204f893f0b5b64bd9eb7032
Michal Cieslakiewicz <michal.cieslakiewicz@wp.pl>
2022-09-07 17:53:02 +0200

    ap/buildah: updated to version 1.27.0.
    Buildah version updated to 1.27.0 to match podman.
    
    Build script fixed to preserve shell profile. Go version selection
    added and set to 1.18.6.
    
    Signed-off-by: Michal Cieslakiewicz <michal.cieslakiewicz@wp.pl>

--- ap/buildah/get_sources.sh
+++ ap/buildah/get_sources.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 SRC=buildah
-# Build requires a proper git tree:
-git clone -b main https://github.com/containers/${SRC}.git $SRC
-VERSION=$(awk '$1=="Version" { print $NF }' $SRC/define/types.go | sed -e 's/-/_/' -e 's/"//g')_$(cd $SRC && git log -n 1 --format=%h --abbrev=8)
-mv $SRC ${SRC}-${VERSION}
-tar cf - ${SRC}-${VERSION} | xz -c9 > ${SRC}-${VERSION}.tar.xz
-[ -s ${SRC}-${VERSION}.tar.xz ] && rm -rf ${SRC}-${VERSION}
+VERSION=1.27.0
+set -x
+wget -c -O ${SRC}-${VERSION}.tar.gz https://github.com/containers/${SRC}/archive/refs/tags/v${VERSION}.tar.gz
