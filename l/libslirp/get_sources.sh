76f43e3488305cf5fd20e7ef2d45e1ffe536416b
Michal Cieslakiewicz <michal.cieslakiewicz@wp.pl>
2022-09-07 15:15:02 +0200

    l/libslirp: updated to 4.7.0.
    Signed-off-by: Michal Cieslakiewicz <michal.cieslakiewicz@wp.pl>

--- l/libslirp/get_sources.sh
+++ l/libslirp/get_sources.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 SRC=libslirp
-VERSION=4.6.1
+VERSION=4.7.0
 set -x
 wget -O ${SRC}-${VERSION}.tar.bz2 https://gitlab.freedesktop.org/slirp/${SRC}/-/archive/v${VERSION}/${SRC}-v${VERSION}.tar.bz2
