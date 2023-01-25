5d061014a80c8bf7d039d77c43744a843759268a
Michal Cieslakiewicz <michal.cieslakiewicz@wp.pl>
2023-01-25 20:55:31 +0100

    d/luajit: switch to git version.
    LuaJit 2.0.5 is quite old and causes current minetest to issue
    warning during compilation, so follow active git version instead.
    
    Signed-off-by: Michal Cieslakiewicz <michal.cieslakiewicz@wp.pl>

--- d/luajit/get_sources.sh
+++ d/luajit/get_sources.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-SRC=LuaJIT
-VERSION=2.0.5
 set -x
-wget -c https://luajit.org/download/${SRC}-${VERSION}.tar.gz
+SRC=luajit
+git clone https://luajit.org/git/${SRC}.git
+VERSION=$(cd $SRC && git log -n 1 --format='%cs_%h' --abbrev=8 | sed -e 's/-//g')
+mv $SRC ${SRC}-${VERSION}
+tar cf - ${SRC}-${VERSION} | xz -c9 > ${SRC}-${VERSION}.tar.xz
+[ -s ${SRC}-${VERSION}.tar.xz ] && rm -rf ${SRC}-${VERSION}
