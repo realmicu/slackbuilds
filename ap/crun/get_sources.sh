27f4234f8af488fae9c39f09d27f779a87f41763
Michal Cieslakiewicz <michal.cieslakiewicz@wp.pl>
2023-04-28 14:14:07 +0200

    ap/crun: updated to 1.8.4.
    Signed-off-by: Michal Cieslakiewicz <michal.cieslakiewicz@wp.pl>

--- ap/crun/get_sources.sh
+++ ap/crun/get_sources.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 SRC=crun
-VERSION=1.5
+VERSION=1.8.4
 set -x
 wget -O ${SRC}-${VERSION}.tar.xz https://github.com/containers/${SRC}/releases/download/${VERSION}/${SRC}-${VERSION}.tar.xz
