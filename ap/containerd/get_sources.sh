7d64226cacc6947eec6737f3186821872b38a6b1
Michal Cieslakiewicz <michal.cieslakiewicz@wp.pl>
2024-04-03 00:45:35 +0200

    ap/containerd added, version 1.7.14.
    containerd is an industry-standard container runtime with an emphasis
    on simplicity, robustness, and portability. It is available as
    a daemon, which can manage the complete container lifecycle of
    its host system: image transfer and storage, container execution
    and supervision, low-level storage and network attachments, etc.
    
    URL: https://containerd.io/
    
    Package includes example config file that changes default runtime
    from 'runc' to 'crun'.
    
    Signed-off-by: Michal Cieslakiewicz <michal.cieslakiewicz@wp.pl>

---
+++ ap/containerd/get_sources.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+SRC=containerd
+BRANCH=main
+set -x
+git clone -b $BRANCH https://github.com/$SRC/$SRC || exit 1
+cd $SRC
+TAGVER=$(git tag --sort=-creatordate | grep -vE -- '-(rc|alpha|beta)' | head -1)
+git checkout $TAGVER
+git prune
+cd $OLDPWD
+VERSION=${TAGVER#v*}
+mv ${SRC} ${SRC}-${VERSION}
+tar cf - ${SRC}-${VERSION} | xz -c9 > ${SRC}-${VERSION}.tar.xz
+[ -s ${SRC}-${VERSION}.tar.xz ] && rm -rf ${SRC}-${VERSION}
