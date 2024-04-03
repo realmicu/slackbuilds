3211bca2a070478d7a760d363b43a73669ed2195
Michal Cieslakiewicz <michal.cieslakiewicz@wp.pl>
2024-04-03 17:04:31 +0200

    l/gumbo-parser: updated.
    Signed-off-by: Michal Cieslakiewicz <michal.cieslakiewicz@wp.pl>

--- l/gumbo-parser/get_sources.sh
+++ l/gumbo-parser/get_sources.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 SRC=gumbo-parser
 BRANCH=master
-git clone -b $BRANCH --depth 1 https://github.com/google/${SRC}.git/ $SRC
-VERSION=$(sed -e 's/\[/ /g' -e 's/\]/ /g' -e 's/,/ /g' -e 's/(/ /g' $SRC/configure.ac | awk -v DATE=$(date '+%Y%m%d') '$1=="AC_INIT" && $2=="gumbo" { printf "%s_%sgit\n", $3, DATE }')
-chown -R root:root $SRC
-mv $SRC ${SRC}-${VERSION}
+set -x
+git clone -b $BRANCH https://codeberg.org/grisha/${SRC}.git $SRC || exit 1
+cd $SRC
+TAGVER=$(git tag --sort=-creatordate | grep -vE -- '-(rc|alpha|beta)' | head -1)
+git checkout $TAGVER
+git prune
+cd $OLDPWD
+VERSION=${TAGVER#v*}
+mv ${SRC} ${SRC}-${VERSION}
 tar cf - ${SRC}-${VERSION} | xz -c9 > ${SRC}-${VERSION}.tar.xz
 [ -s ${SRC}-${VERSION}.tar.xz ] && rm -rf ${SRC}-${VERSION}
