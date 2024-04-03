8ed6c128fefc1b885493c28fe61ed77cd41fc71b
Michal Cieslakiewicz <michal.cieslakiewicz@wp.pl>
2024-04-03 17:05:54 +0200

    l/litehtml: updated, uses external gumbo-parser now.
    Signed-off-by: Michal Cieslakiewicz <michal.cieslakiewicz@wp.pl>

--- l/litehtml/get_sources.sh
+++ l/litehtml/get_sources.sh
@@ -1,6 +1,14 @@
-APP=litehtml
-VERSION=$(date '+%Y%m%d')git
-git clone --depth=1 https://github.com/litehtml/${APP}.git
-mv ${APP} ${APP}-${VERSION} 
-tar cf - ${APP}-${VERSION} | xz -c9 > ${APP}-${VERSION}.tar.xz
-[ -s ${APP}-${VERSION}.tar.xz ] && rm -rf ${APP}-${VERSION}
+#!/bin/bash
+SRC=litehtml
+BRANCH=master
+set -x
+git clone -b $BRANCH https://github.com/$SRC/${SRC}.git $SRC || exit 1
+cd $SRC
+TAGVER=$(git tag --sort=-creatordate | grep -vE -- '-(rc|alpha|beta)' | head -1)
+git checkout $TAGVER
+git prune
+cd $OLDPWD
+VERSION=${TAGVER#v*}
+mv ${SRC} ${SRC}-${VERSION}
+tar cf - ${SRC}-${VERSION} | xz -c9 > ${SRC}-${VERSION}.tar.xz
+[ -s ${SRC}-${VERSION}.tar.xz ] && rm -rf ${SRC}-${VERSION}
