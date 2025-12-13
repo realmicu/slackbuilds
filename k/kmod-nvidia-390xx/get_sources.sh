df9c08f2b06ab158267560c0b4b284b06275776d
Michal Cieslakiewicz <michal.cieslakiewicz@wp.pl>
2025-12-13 18:31:07 +0100

    Add series suffix (390xx) to Nvidia legacy software.

--- k/kmod-nvidia-390xx/get_sources.sh
+++ k/kmod-nvidia-390xx/get_sources.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 BINDIST=NVIDIA-Linux-x86_64
-SRC=$(ls ../../x/nvidia-driver/${BINDIST}-*.run 2>/dev/null | tail -1)
+SRC=$(ls ../../x/nvidia-driver-390xx/${BINDIST}-*.run 2>/dev/null | tail -1)
 [ ! -z "$SRC" ] && [ -s "$SRC" ] && ln -s $SRC .
