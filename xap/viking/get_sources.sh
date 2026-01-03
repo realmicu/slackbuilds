a06088e4b98b4f559144cc0235a68d73f48a3487
Michal Cieslakiewicz <michal.cieslakiewicz@wp.pl>
2026-01-03 17:01:30 +0100

    xap/viking: updated to version 1.10.

--- xap/viking/get_sources.sh
+++ xap/viking/get_sources.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-SRC=viking
-VERSION=1.8
-set -x
-wget -c https://github.com/viking-gps/${SRC}/archive/${SRC}-${VERSION}.tar.gz || exit 1
-tar xf ${SRC}-${VERSION}.tar.gz && rm -f ${SRC}-${VERSION}.tar.gz
-mv ${SRC}-${SRC}-${VERSION} ${SRC}-${VERSION} 
-tar cf - ${SRC}-${VERSION} | xz -c9 > ${SRC}-${VERSION}.tar.xz
-[ -s ${SRC}-${VERSION}.tar.xz ] && rm -rf ${SRC}-${VERSION}
+APP=viking
+VERSION=1.10
+set -ex
+wget -c -O ${APP}-${VERSION}.tar.bz2 https://sourceforge.net/projects/${APP}/files/${APP}-${VERSION}.tar.bz2/download
