9b7517b3ba8bd45b2f8a9598bacaa975779a3201
Michal Cieslakiewicz <michal.cieslakiewicz@wp.pl>
2026-01-30 23:20:55 +0100

    x/nvidia-driver-580xx: fixes to suspend, libs, config etc.

--- x/nvidia-driver-580xx/doinst.sh
+++ x/nvidia-driver-580xx/doinst.sh
@@ -1,9 +1,19 @@
-( cd usr/lib${LIBSUFFIX}/xorg/modules/extensions
-  if [ -f libglx.so ] ; then
-    if [ -L libglx.so ] ; then rm -f libglx.so
-    else mv libglx.so libglx.so-xorg ; fi
-    ln -s libglx.so.$VERSION libglx.so
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  # If there's no config file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    # toss the redundant copy
+    rm $NEW
   fi
-)
+  # Otherwise, we leave the .new copy for the admin to consider...
+}
+
+config etc/X11/xorg.conf.d/10-nvidia.conf.new
+config etc/ld.so.conf.d/nvidia.conf.new
+config etc/modprobe.d/nvidia.conf.new
+
 [ -x sbin/ldconfig ] && sbin/ldconfig -X -C etc/ld.so.cache -f etc/ld.so.conf
 [ -x usr/bin/update-desktop-database ] && usr/bin/update-desktop-database -q usr/share/applications
--- x/nvidia-driver-580xx/douninst.sh
+++ x/nvidia-driver-580xx/douninst.sh
@@ -1,8 +1,4 @@
-( cd usr/lib${LIBSUFFIX}
+( cd usr/lib${LIBDIRSUFFIX}
   [ -f libGLX_indirect.so.0 ] && rm -f libGLX_indirect.so.0
   [ -f libGLX_mesa.so.0.0.0 ] && ln -s libGLX_mesa.so.0.0.0 libGLX_indirect.so.0
-)
-( cd usr/lib${LIBSUFFIX}/xorg/modules/extensions
-  [ -f libglx.so ] && rm -f libglx.so
-  [ -f libglx.so-xorg ] && mv libglx.so-xorg libglx.so
 )
