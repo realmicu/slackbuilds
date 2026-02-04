495eb4afdfd2b23fda6f7bc542c43a422fa1d584
Michal Cieslakiewicz <michal.cieslakiewicz@wp.pl>
2026-02-04 20:22:27 +0100

    xap/viking: updated to version 1.11.

--- xap/viking/get_sources.sh
+++ xap/viking/get_sources.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-APP=viking
-VERSION=1.10
 set -ex
-wget -c -O ${APP}-${VERSION}.tar.bz2 https://sourceforge.net/projects/${APP}/files/${APP}-${VERSION}.tar.bz2/download
+APP=viking
+GITURL="https://github.com/viking-gps/viking"
+RELURL="https://sourceforge.net/projects/${APP}/files"
+VERSION="$(git ls-remote --tags --refs --sort=-v:refname $GITURL refs/tags/viking-[0-9]* | head -1 | rev | cut -d- -f1 | rev)"
+# We check latest version tag using git but download release archive:
+wget -c -O ${APP}-${VERSION}.tar.bz2 "${RELURL}/${APP}-${VERSION}.tar.bz2/download"
