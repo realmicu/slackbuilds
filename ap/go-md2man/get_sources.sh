dff751b366e3f626e08d172a50a6e68102fbf23d
Michal Cieslakiewicz <michal.cieslakiewicz@wp.pl>
2026-06-16 17:18:12 +0200

    ap/go-md2man: updated.
    Signed-off-by: Michal Cieslakiewicz <michal.cieslakiewicz@wp.pl>

--- ap/go-md2man/get_sources.sh
+++ ap/go-md2man/get_sources.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-SRC=go-md2man
-VERSION=2.0.2
-set -x
-wget -O ${SRC}-${VERSION}.tar.gz https://github.com/cpuguy83/${SRC}/archive/v${VERSION}.tar.gz
+set -ex
+APP="go-md2man"
+URL="https://github.com/cpuguy83/${APP}"
+TAGVER="$(git ls-remote --tags --refs --sort=-v:refname $URL refs/tags/{,v}[0-9]* | rev | cut -d/ -f1 | rev | head -1)"
+VERSION="$(tr -d 'v' <<< $TAGVER)"
+git clone -b $TAGVER --single-branch --recurse-submodules $URL $APP-$VERSION
+tar cf - ${APP}-${VERSION} | xz -c9 > ${APP}-${VERSION}.tar.xz
+[ -s ${APP}-${VERSION}.tar.xz ] && rm -rf ${APP}-${VERSION}
