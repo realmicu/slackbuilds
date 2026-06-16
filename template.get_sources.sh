73bdbbb9fd494282df873043d6dab8911d5bc00e
Michal Cieslakiewicz <michal.cieslakiewicz@wp.pl>
2026-06-16 17:15:05 +0200

    Make template.get_sources.sh more universal.
    Signed-off-by: Michal Cieslakiewicz <michal.cieslakiewicz@wp.pl>

--- template.get_sources.sh
+++ template.get_sources.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -ex
-APP=Application_name	# change
-URL=Repository_URL	# change
-VERSION="$(git ls-remote --tags --refs --sort=-v:refname $URL refs/tags/[0-9]* | head -1 | rev | cut -d/ -f1 | rev)"
-git clone -b $VERSION --single-branch $URL $APP
-mv $APP $APP-$VERSION
+APP="Application_name"	# change
+URL="Repository_URL"	# change
+TAGVER="$(git ls-remote --tags --refs --sort=-v:refname $URL refs/tags/{,v}[0-9]* | rev | cut -d/ -f1 | rev | head -1)"
+VERSION="$(tr -d 'v' <<< $TAGVER)"
+git clone -b $TAGVER --single-branch --recurse-submodules $URL $APP-$VERSION
 tar cf - ${APP}-${VERSION} | xz -c9 > ${APP}-${VERSION}.tar.xz
 [ -s ${APP}-${VERSION}.tar.xz ] && rm -rf ${APP}-${VERSION}
