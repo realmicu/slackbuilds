beea705798c7a3ae8627399012aea483ac875cb6
Michal Cieslakiewicz <michal.cieslakiewicz@wp.pl>
2026-06-16 17:26:49 +0200

    cntr/conmon: updated.
    Signed-off-by: Michal Cieslakiewicz <michal.cieslakiewicz@wp.pl>

--- cntr/conmon/get_sources.sh
+++ cntr/conmon/get_sources.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-SRC=conmon
-VERSION=2.1.7
-set -x
-wget -O ${SRC}-${VERSION}.tar.gz https://github.com/containers/${SRC}/archive/v${VERSION}.tar.gz
+set -ex
+APP="conmon"
+URL="https://github.com/containers/$APP"
+TAGVER="$(git ls-remote --tags --refs --sort=-v:refname $URL refs/tags/{,v}[0-9]* | rev | cut -d/ -f1 | rev | head -1)"
+VERSION="$(tr -d 'v' <<< $TAGVER)"
+git clone -b $TAGVER --single-branch --recurse-submodules $URL $APP-$VERSION
+tar cf - ${APP}-${VERSION} | xz -c9 > ${APP}-${VERSION}.tar.xz
+[ -s ${APP}-${VERSION}.tar.xz ] && rm -rf ${APP}-${VERSION}
