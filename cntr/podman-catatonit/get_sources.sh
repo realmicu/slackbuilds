ff668e255bafd3d5bf32d77d7b212a810bc978ae
Michal Cieslakiewicz <michal.cieslakiewicz@wp.pl>
2026-06-29 21:24:11 +0200

    cntr/podman-catatonit: updated.
    Signed-off-by: Michal Cieslakiewicz <michal.cieslakiewicz@wp.pl>

--- cntr/podman-catatonit/get_sources.sh
+++ cntr/podman-catatonit/get_sources.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-SRC=catatonit
-VERSION=0.1.7
-set -x
-wget -c -O ${SRC}-${VERSION}.tar.gz https://github.com/openSUSE/${SRC}/archive/refs/tags/v${VERSION}.tar.gz
+set -ex
+SRC="catatonit"
+APP="podman-$SRC"
+URL="https://github.com/openSUSE/$SRC"
+TAGVER="$(git ls-remote --tags --refs --sort=-v:refname $URL refs/tags/v[0-9]* | rev | cut -d/ -f1 | rev | head -1)"
+VERSION="$(tr -d 'v' <<< $TAGVER)"
+git clone -b $TAGVER --single-branch --recurse-submodules $URL $APP-$VERSION
+tar cf - ${APP}-${VERSION} | xz -c9 > ${APP}-${VERSION}.tar.xz
+[ -s ${APP}-${VERSION}.tar.xz ] && rm -rf ${APP}-${VERSION}
