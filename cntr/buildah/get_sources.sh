b42b773a9f6e6a74bd99fe98ea7533c1b997a273
Michal Cieslakiewicz <michal.cieslakiewicz@wp.pl>
2026-06-29 21:02:50 +0200

    cntr/buildah: updated to version 1.33.8.
    Signed-off-by: Michal Cieslakiewicz <michal.cieslakiewicz@wp.pl>

--- cntr/buildah/get_sources.sh
+++ cntr/buildah/get_sources.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 SRC=buildah
-VERSION=1.30.0
+# Match podman - 'podman system info | fgrep -w buildahVersion':
+VERSION="1.33.8"
 set -x
 wget -c -O ${SRC}-${VERSION}.tar.gz https://github.com/containers/${SRC}/archive/refs/tags/v${VERSION}.tar.gz
